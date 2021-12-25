471a6b60137dd1973914e13babaab1ee5d19fbd4
tonykafe <tony@kafe.rocks>
2021-12-25 23:43:15 +0100

    Setting up GA4

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,27 @@ module.exports = {
   plugins: [
     "gatsby-plugin-emotion",
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-N7VFFQ6ENG",
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-N7VFFQ6ENG", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command.
+        // This config will be shared across all trackingIds.
+        gtagConfig: {
+          // Anonymizes the last digits of the user’s IP.
+          // To comply with policies and legal regulations.
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin.
+        pluginConfig: {
+          // As false it puts the tracking script in the body instead of the head.
+          head: false,
+          // Optional parameter to honor the Do Not Track feature.
+          respectDNT: true,
+        },
       },
     },
     "gatsby-plugin-image",
