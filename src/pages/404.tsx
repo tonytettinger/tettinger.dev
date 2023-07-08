cd8cfdbac90451547897d7a5709c9cfb10fd54fd
Tony Tettinger <antal.tettinger@gmail.com>
2023-07-09 01:21:45 +0200

    chore: updates, fixes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 
 // styles
 const pageStyles = {
-    color: '#232129',
+    color: '#FFF',
     padding: '96px',
     fontFamily: '-apple-system, Roboto, sans-serif, serif',
 }
@@ -16,13 +16,6 @@ const headingStyles = {
 
 const paragraphStyles = {
     marginBottom: 48,
-}
-const codeStyles = {
-    color: '#8A6534',
-    padding: 4,
-    backgroundColor: '#FFF4DB',
-    fontSize: '1.25rem',
-    borderRadius: 4,
 }
 
 // markup
@@ -37,15 +30,6 @@ const NotFoundPage = () => {
                     😔
                 </span>{' '}
                 we couldn’t find what you were looking for.
-                <br />
-                {process.env.NODE_ENV === 'development' ? (
-                    <>
-                        <br />
-                        Try creating a page in <code style={codeStyles}>src/pages/</code>.
-                        <br />
-                    </>
-                ) : null}
-                <br />
                 <Link to="/">Go home</Link>.
             </p>
         </main>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,23 +6,11 @@ const AboutPage = () => {
     return (
         <VStack>
             <Heading as="h1">About me</Heading>
-            <Text>
-                I'm a Full Stack Engineer with wide range of experience in developing web
-                applications, finding maintainable solutions with front and back-end technologies
-                that have proven to increase performance, ranking, user experience and in general
-                contributed to bottom line results and business goals for the companies I have
-                worked with.
-            </Text>
-            <Text>
-                My main areas of expertise are JavaScript, TypeScript, ReactJS, PHP, HTML5, CSS3 and
-                I have some work experience with Python, serverles functions, GraphQL and SQL.
-            </Text>
 
             <Text>
-                I have been using BEM and SMACSS based CSS and SCSS and also utility-first CSS
-                frameworks in my projects. I'm striving for simplicity, maintainable code and
-                providing robust solutions. I have developed applications and solved issues that
-                require a comprehensive knowledge of web technologies. I'm a believer of continuous
+                As a developer I'm striving for simplicity, maintainable code and providing robust
+                solutions. I have developed applications and solved issues that require a
+                comprehensive knowledge of web technologies. I'm a believer of continuous
                 self-improvement and the principles of YAGNI, KISS. No over-engineering, no
                 over-abstraction, listen to others, take criticism and improve continuously.
             </Text>
