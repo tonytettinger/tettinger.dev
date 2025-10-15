9d739d8561eac5da399b0427682548744d9900b1
tonytettinger <antal.tettinger@gmail.com>
2025-10-15 23:24:25 +0200

    feat: refactor index page layout and improve homepage introduction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ import { Box, Icon, Text, VStack } from '@chakra-ui/react'
 
 import { DiCss3, DiHtml5, DiJavascript1, DiReact } from 'react-icons/di'
 
+import { PostList } from '../components/PostList'
 import Wave from '../components/motion/Wave'
 import { usePosts } from '../providers/postProvider'
 import { type Node } from '../types'
-import { PostList } from '../components/PostList'
 
 const iconsOverImage = [
     { icon: DiHtml5 },
@@ -19,57 +19,56 @@ const iconsOverImage = [
 ]
 
 const IndexPage = () => {
-const posts: Node[] = usePosts('projects')
-const sortedPosts = posts.sort((a, b) => a.frontmatter.order - b.frontmatter.order)
-    
- return (
-    <>
-        <Box style={{ position: 'relative' }}>
-            <StaticImage
-                alt="Tony by the ocean"
-                src="../../static/images/tony_tettinger.jpeg"
-                placeholder="blurred"
-                quality={100}
-                loading="eager"
-                style={{
-                    width: '100%',
-                    maxHeight: '320px',
-                    borderRadius: '0.5rem',
-                    objectFit: 'contain',
-                }}
-            />
-            <Box
-                style={{
-                    position: 'absolute',
-                    right: '8%',
-                    top: '40%',
-                    backgroundColor: 'transparent',
-                    display: 'flex',
-                }}
-            >
-                {iconsOverImage.map(({ icon: IconComponent }, index) => (
-                    <Wave key={index} delay={0.3 * index}>
-                        <Icon
-                            as={IconComponent}
-                            w={{ base: 10, md: 14 }}
-                            h={{ base: 10, md: 14 }}
-                            color="gray.900"
-                        />
-                    </Wave>
-                ))}
-            </Box>
-        </Box>
-        <VStack spacing={4} my={4}>
-            <Text>
-                Welcome to my homepage!
+    const posts: Node[] = usePosts('projects')
+    const sortedPosts = posts.sort((a, b) => a.frontmatter.order - b.frontmatter.order)
 
-                I'm <b>Antal 'Tony' Tettinger</b>, a Berlin-based <b>AI Frontend Engineer with an MSc in Full Stack Software Engineering</b>. 
-                I have a product-minded approach that focuses on building delightful user experiences and robust solutions. 
-                I believe in continuous self-improvement and leveraging technology to solve meaningful problems.
-            </Text>
-            <PostList posts={sortedPosts} title="Projects" />
-        </VStack>
-    </>
-)
+    return (
+        <>
+            <Box style={{ position: 'relative' }}>
+                <StaticImage
+                    alt="Tony by the ocean"
+                    src="../../static/images/tony_tettinger.jpeg"
+                    placeholder="blurred"
+                    quality={100}
+                    loading="eager"
+                    style={{
+                        width: '100%',
+                        maxHeight: '320px',
+                        borderRadius: '0.5rem',
+                        objectFit: 'contain',
+                    }}
+                />
+                <Box
+                    style={{
+                        position: 'absolute',
+                        right: '8%',
+                        top: '40%',
+                        backgroundColor: 'transparent',
+                        display: 'flex',
+                    }}
+                >
+                    {iconsOverImage.map(({ icon: IconComponent }, index) => (
+                        <Wave key={index} delay={0.3 * index}>
+                            <Icon
+                                as={IconComponent}
+                                w={{ base: 10, md: 14 }}
+                                h={{ base: 10, md: 14 }}
+                                color="gray.900"
+                            />
+                        </Wave>
+                    ))}
+                </Box>
+            </Box>
+            <VStack spacing={4} my={4}>
+                <Text>
+                    Welcome to my homepage! I'm <b>Antal 'Tony' Tettinger</b>, a Berlin-based{' '}
+                    <b>AI Frontend Engineer with an MSc in Full Stack Software Engineering</b>. I
+                    have a product-minded approach that focuses on building delightful user
+                    experiences and robust solutions. I believe in continuous self-improvement and
+                    leveraging technology to solve meaningful problems.
+                </Text>
+            </VStack>
+        </>
+    )
 }
 export default IndexPage
